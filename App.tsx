146a33017e169654331e2523011c27bf51524fd2
prasann278 <prasanndubey@outlook.com>
2023-04-26 03:00:31 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import MainContainer from './src/MainContainer';
+import { store } from './src/redux/store/Store';
+
+
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <MainContainer />
+    </Provider>
+  ); 
+};
+
+export default App;
